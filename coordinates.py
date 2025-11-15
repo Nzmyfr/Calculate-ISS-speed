fa66c81ba3ad94186942e280cf8f423ee026a593
Aleks Veselinov <veselinovaleksandar7@gmail.com>
2025-11-15 22:08:05 +0200

    Update coordinate.py

--- coordinates.py
+++ coordinates.py
@@ -1,10 +1,30 @@
 from astro_pi_orbit import ISS
 from datetime import UTC, datetime
 from skyfield.api import load
+from time import sleep
+
+time = 0.5 # seconds
 
 iss = ISS()
 time_scale = load.timescale()
-time = datetime.now(UTC)
-position = iss.at(time_scale.from_datetime(time))
-# print(iss.coordinates(), position.km)
-dir(position)
+
+average = 0
+count 	= 0
+for _ in range(100):
+    time_1 = datetime.now(UTC)
+    moment_1 = time_scale.from_datetime(time_1)
+    position_1 = iss.at(moment_1)
+    sleep(time)
+    time_2 = datetime.now(UTC)
+    moment_2 = time_scale.from_datetime(time_2)
+    position_2 = iss.at(moment_2)
+
+    diff = position_2 - position_1
+    distance_km = diff.distance().km
+    velocity = distance_km / time
+    print(f'{velocity:.4f}')
+    
+    average = (count * average + velocity) / (count + 1)
+    count += 1
+
+print(f'The average velocity of the ISS is {average:.4f}')
