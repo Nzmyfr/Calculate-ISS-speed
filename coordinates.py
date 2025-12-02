39f434608350341d06d246e1fa2024e390c196d7
Nzmyfr <alexvtonev@gmail.com>
2025-12-02 18:02:28 +0200

    add  finding gsd in the code

--- coordinates.py
+++ coordinates.py
@@ -10,6 +10,7 @@ time_scale = load.timescale()
 
 average = 0
 count 	= 0
+altitude_values = []
 for _ in range(100):
     time_1 = datetime.now(UTC)
     moment_1 = time_scale.from_datetime(time_1)
@@ -24,9 +25,18 @@ for _ in range(100):
     diff = position_2 - position_1
     distance_km = diff.distance().km
     velocity = distance_km / time
+    altitude = position_2.distance().m
+    altitude_values.append(altitude)
     print(f'{velocity:.4f}')
+    print(f'Altitude: {altitude:.4f} m')
     
     average = (count * average + velocity) / (count + 1)
     count += 1
 
 print(f'The average velocity of the ISS is {average:.4f}')
+
+def find_gsd(altitude, focal_length=35):
+    focal_length = 35  # in mm
+    sensor_width = 7.564  # in mm for Raspberry Pi Camera
+    gsd = sensor_width * altitude / (focal_length * 1000)  # in meters per pixel
+    return gsd
